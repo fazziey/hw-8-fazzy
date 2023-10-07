638bde2c8ff17cb028b271e067b96b9638e38346
Fazzy F.r <fazzyfitraturrahman93@gmail.com>
2023-10-07 11:41:14 +0700

    homework week 8

---
+++ db/seeding.js
@@ -0,0 +1,10 @@
+const { log } = require("console");
+const pool = require("../queries.js");
+const fs = require("fs");
+
+const seedQuery = fs.readFileSync("db/seeding.sql", { encoding: "utf-8" });
+pool.query(seedQuery, (err, res) => {
+  console.log(err);
+  console.log("seeding complete");
+  pool.end;
+});
---
+++ db/seeding.sql
@@ -0,0 +1,6 @@
+INSERT INTO actor(first_name, last_name, last_update)
+VALUES  ('Albert', 'Finney', NOW()),
+        ('Bill', 'Nighy', NOW()),
+        ('Rosamund', ' Pike', NOW()),
+        ('Lea', 'Thompson', NOW()),
+        ('Joe', 'Pantoliano', NOW());
